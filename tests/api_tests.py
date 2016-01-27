861eb12d95a33db1e6ab4c8989ff2f37d81d6cb7
fernando <nanocatdemen@gmail.com>
2016-01-27 12:26:31 -0300

    Update api tests

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -1,10 +1,18 @@
 from . import base_test_case
-
+from app import app
+from datetime import datetime
 
 class APITestCase(base_test_case.BaseTestCase):
     '''
     Unit tests for the API
     '''
     def test_registration(self):
-        request = self.app.post('/api/registration')
-        assert request.status_code == 201
+        from app.api.models.sim import Sim
+        with app.app_context():
+            date = datetime.now().date()
+            request = self.app.post('/api/registration')
+            assert request.status_code == 201
+            Sim = Sim.query.all()
+            assert len(Sim) == 1
+            assert Sim[0].serial_number == 1
+            assert Sim[0].creation_date == date
