818a3a668d260c4526d4613f56b6a3efc228415e
Fernando Morales <fgmoralesc@gmail.com>
2016-01-27 10:31:37 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    USER = 'batman
+    USER = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = 'postgresql://' + USER + ':' + SECRET_KEY + '@localhost/adkintun'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
