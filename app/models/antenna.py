37f9eaaa78b52beb376bb86b2c85efce3e31b338
zeeerooo <boris.rq@gmail.com>
2016-03-08 16:40:09 -0300

    Reparada clase antena, agregando carrier.
    close #17

--- app/models/antenna.py
+++ app/models/antenna.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, Float
+from sqlalchemy.sql.schema import ForeignKey
 from . import base_model
 
 
@@ -6,16 +7,20 @@ class Antenna(base_model.BaseModel):
     '''
     Clase antena.
     '''
-    __tablename__='antennas'
+    __tablename__ = 'antennas'
     id = Column(Integer, primary_key=True)
     cid = Column(Integer)
     lac = Column(Integer)
     lat = Column(Float)
     lng = Column(Float)
+    carrier = Column(Integer, ForeignKey("carriers.id"))
 
-    def __init__(self, cid, lac):
+    def __init__(self, cid, lac, lat, long, carrier):
         self.cid = cid
         self.lac = lac
+        self.lat = lat
+        self.long = long
+        self.carrier = carrier
 
     def __repr__(self):
-        return '<Antenna, cid: %r, lac: %r>' % (self.cid, self.lac)
+        return '<Antenna, id: %r, carrier: %r, cid: %r, lac: %r>' % (self.id, self.carrier, self.cid, self.lac)
