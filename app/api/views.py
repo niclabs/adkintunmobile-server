63f475b7aa045487c2e416f0953d30204afd7a00
JoseTapiaG <jose.wt@gmail.com>
2016-03-16 15:28:32 -0300

    leer archivo zip y extraer contenido

--- app/api/views.py
+++ app/api/views.py
@@ -1,6 +1,9 @@
+import werkzeug
+from flask import request
 from flask_restful import Resource, reqparse
 from .. import db
 from . import api
+from . import app
 from datetime import datetime
 
 
@@ -63,5 +66,29 @@ class Registration(Resource):
         else:
             return 'Carrier no existe', 400
 
+
+class SaveEvents(Resource):
+    def post(self):
+        post_parser = reqparse.RequestParser(bundle_errors=True)
+        post_parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
+
+        args = post_parser.parse_args()
+        pass
+
+
+import zipfile
+
+@app.route("/send_file", methods=['POST'])
+def read_events():
+    f = request.files['pic']
+
+    z = zipfile.ZipFile(f)
+    for name in z.namelist():
+        json = z.open(name)
+        lines = json.readlines()
+        string = ' '.join(str(x) for x in lines)
+        print(string)
+
+    return f
 
 api.add_resource(Registration, '/api/registration')
