3704270d676a4fdf7094973ff7d470a5f358411b
zeeerooo <boris.rq@gmail.com>
2016-03-22 12:30:59 -0300

    Modificado admin para ver Sims, Devices y Carriers

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,24 +1,33 @@
 from app.models.sim import Sim
+from app.models.carrier import Carrier
+from app.models.device import Device
 
-from flask_admin.contrib import sqla
+from flask_admin.contrib.sqla import ModelView
 from . import admin
-from .. import db
+from app import db
 
 
-class SimView(sqla.ModelView):
+class SimView(ModelView):
     column_display_pk = True
+    create_modal = True
 
-# class DeviceView(sqla.ModelView):
-#     column_display_pk = True
-#     column_hide_backrefs = False
-#     column_list = ('id', 'sdk', 'mobile_plan')
+class CarrierView(ModelView):
+    column_display_pk = True
+    create_modal = True
+    column_exclude_list = ['id', ]
+
+class DeviceView(ModelView):
+     column_display_pk = True
+     # column_hide_backrefs = False
+     # column_list = ('id', 'sdk', 'mobile_plan')
 
 # class AntennaView(sqla.ModelView):
 #     pass
 
 # Add views
 # admin.add_view(UserView(User, db.session))
-# admin.add_view(DeviceView(Device, db.session))
+admin.add_view(DeviceView(Device, db.session))
 # admin.add_view(sqla.ModelView(Antenna, db.session))
 admin.add_view(SimView(Sim, db.session))
 # admin.add_view(sqla.ModelView(Mobile_Plan, db.session))
+admin.add_view(CarrierView(Carrier, db.session))
