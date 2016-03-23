3d1a4685eb52243beff8a4dc6b4f11f71e5307bb
JoseTapiaG <jose.wt@gmail.com>
2016-03-23 16:55:42 -0300

    Test de persistencia de eventos

--- app/api/events_view.py
+++ app/api/events_view.py
@@ -1,9 +1,12 @@
 from flask import request
 
+from app.models.device import Device
+from app.models.sim import Sim
 from . import app
 from . import api
 from flask_restful import Resource, reqparse
 from .. import db
+from datetime import datetime
 
 
 class ReadEvents(Resource):
@@ -14,14 +17,18 @@ class ReadEvents(Resource):
 
         args = post_parser.parse_args()
         jsonvar = json.loads(args.events)
+        device = Device.query.filter(Device.build_id == jsonvar["device_records"]["build_id"]).first()
+        sim = Sim.query.filter(Sim.serial_number == jsonvar["sim_records"]["serial_number"]).first()
+        del jsonvar["device_records"]
+        del jsonvar["sim_records"]
+        try:
+            for events_name, events in jsonvar.items():
+                save(events_name, events, device, sim)
 
-        # TODO tomar device del json
-        device = None
-        for events in jsonvar:
-            events_name = None
-            save_events(events_name, events, device)
-
-        return "hola"
+        except Exception as e:
+            db.session.rollback()
+            return e, 400
+        return '', 201
 
 
 @app.route("/send_file", methods=['POST'])
@@ -33,28 +40,36 @@ def read_events():
     string = ''.join(x.decode("utf-8") for x in lines)
     string = string.replace('\n', '')
     jsonvar = json.loads(string)
+    device = Device.query.filter(Device.device == jsonvar["device_records"]["build_id"]).first()
+    sim = Sim.query.filter(Sim.serial_number == jsonvar["sim_records"]["serial_number"]).first()
+    del jsonvar["device_records"]
+    del jsonvar["sim_records"]
+    try:
+        for events in jsonvar:
+            events_name = None
+            save(events_name, events, device, sim)
 
-    # TODO tomar device del json
-    device = None
-    for events in jsonvar:
-        events_name = None
-        save(events_name, events, device)
-
-    return "hola"
+    except Exception as e:
+        db.session.rollback()
+        return e, 400
+    return 'Eventos guardados', 201
 
 
 api.add_resource(ReadEvents, '/api/send_file')
 
 
 def save_events(model):
-    def save_event_with_model(events, device):
+    def save_event_with_model(events, device, sim):
         for event in events:
             eventModel = model()
-            for k, v in event:
-                setattr(eventModel, k, v)
-
+            for k, v in event.items():
+                if hasattr(eventModel, k):
+                    if k == "timestamp":
+                        v = datetime.fromtimestamp(timestamp=v)
+                    setattr(eventModel, k, v)
             device.events.append(eventModel)
-            device.sim.events.append(eventModel)
+            sim.events.append(eventModel)
+            db.session.add(sim)
             db.session.add(eventModel)
             db.session.add(device)
             db.session.commit()
@@ -63,21 +78,24 @@ def save_events(model):
 
 
 def save_observations(model):
-    def save_event_with_model(events, device):
+    def save_event_with_model(events, device, sim):
         for event in events:
             eventModel = model()
-            for k, v in event:
-                setattr(eventModel, k, v)
+            for k, v in event.items():
+                if hasattr(eventModel, 'k'):
+                    if k == "timestamp":
+                        v = datetime.fromtimestamp(timestamp=v)
+                    setattr(eventModel, k, v)
 
             device.events.append(eventModel)
-            device.sim.events.append(eventModel)
-            device.sim.carrier.telephony_observation_events.append(eventModel)
+            sim.events.append(eventModel)
+            sim.carrier.telephony_observation_events.append(eventModel)
+            db.session.add(sim)
             db.session.add(eventModel)
             db.session.add(device)
             db.session.commit()
 
     return save_event_with_model
-
 
 
 from app.models.application_traffic_event import ApplicationTrafficEvent
@@ -86,18 +104,19 @@ from app.models.connectivity_event import ConnectivityEvent
 from app.models.gsm_event import GsmEvent
 from app.models.mobile_traffic_event import MobileTrafficEvent
 from app.models.state_change_event import StateChangeEvent
+from app.models.traffic_event import TrafficEvent
 from app.models.wifi_traffic_event import WifiTrafficEvent
 
 events_names = {
-    'traffic': save_events(ApplicationTrafficEvent),
-    'cdma': save_observations(CdmaEvent),
+    'traffic_records': save_events(ApplicationTrafficEvent),
+    'cdma_records': save_observations(CdmaEvent),
     'connectivity': save_events(ConnectivityEvent),
-    'gsm': save_observations(GsmEvent),
-    'mobile': save_events(MobileTrafficEvent),
-    'state': save_events(StateChangeEvent),
-    'wifi': save_events(WifiTrafficEvent)
+    'gsm_records': save_observations(GsmEvent),
+    'telephony_records': save_events(MobileTrafficEvent),
+    'state_records': save_events(StateChangeEvent),
+    'wifi_records': save_events(WifiTrafficEvent)
 }
 
 
-def save(events_name, events, device):
-    events_names[events_name](events, device)
+def save(events_name, events, device, sim):
+    events_names[events_name](events, device, sim)
