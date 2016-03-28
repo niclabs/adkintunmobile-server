4c7ed5539284531d39a843629678bf40bf70b0e4
zeeerooo <boris.rq@gmail.com>
2016-03-28 16:59:37 -0300

    Merge en rama dev

--- app/models/traffic_event.py
+++ app/models/traffic_event.py
@@ -7,7 +7,7 @@ class TrafficEvent(Event):
     Clase para los eventos de trafico
     '''
     __tablename__ = 'traffic_events'
-    __mapper_args__ = {'polymorphic_identity': 'Traffic_event'}
+    # __mapper_args__ = {'polymorphic_identity': 'Traffic_event'}
 
     id = db.Column(db.Integer, db.ForeignKey('events.id'), primary_key=True)
     network_type = db.Column(db.Integer)
@@ -18,6 +18,6 @@ class TrafficEvent(Event):
     tcp_rx_bytes = db.Column(db.BigInteger)
     tcp_tx_bytes = db.Column(db.BigInteger)
 
-    # Herencia
-    type_traffic_event = db.Column(db.String(50))
-    __mapper_args__ = {'polymorphic_on': type_traffic_event}
+    # # Herencia
+    # type_traffic_event = db.Column(db.String(50))
+    # __mapper_args__ = {'polymorphic_on': type_traffic_event}
