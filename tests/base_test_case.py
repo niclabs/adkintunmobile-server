75b6e36d693d0ecc288c1e9c847ccb3eefc2281c
zeeerooo <boris.rq@gmail.com>
2016-03-29 12:30:45 -0300

    Cambi api-test por resgistration-api

--- tests/base_test_case.py
+++ tests/base_test_case.py
@@ -1,18 +1,12 @@
 import unittest
-
 from app import app, db
-from app.models.sim import Sim
-from app.models.carrier import Carrier
-from dateutil.parser import parse
+
 
 class BaseTestCase(unittest.TestCase):
 
-    pass
-    __test__ = False
-
-    sim = {
-        'creation_date': '04/02/2016',
-    }
+    def tearDown(self):
+        db.drop_all(bind=None)
+        self.context.pop()
 
     def setUp(self, populate=True):
         # Load testing configuration
@@ -27,21 +21,3 @@ class BaseTestCase(unittest.TestCase):
         # load data
         if (populate):
             self.populate()
-
-    def populate(self):
-        # TODO: Ponerle un nombre más representativo del test en el cual se usará (tiene sentido que esté en esta clase?)
-        '''
-        Populate the model with test data
-        '''
-        # Create the default sim
-        carrier = Carrier("test", 1, 456)
-        sim = Sim(creation_date=parse(self.sim.get('creation_date')), serial_number=1234)
-        try:
-            db.session.add(carrier)
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-
-    def tearDown(self):
-        db.drop_all(bind=None)
-        self.context.pop()
