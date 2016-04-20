43d29bd61806b8d0838232118aefe85d6cc6ebae
JoseTapiaG <jose.wt@gmail.com>
2016-04-20 17:27:22 -0300

    Primera mitad de registros de estaisticas generales

--- app/report/report.py
+++ app/report/report.py
@@ -1,2 +1,29 @@
+
+
 def generate_report():
     print("Generar reporte")
+
+
+# Total de equipos que han entregado datos
+def totalDevicesReported():
+    from app.models.device import Device
+    return Device.query.filter(Device.events != None).count()
+
+# Total de sims registradas
+def totalSimsRegistered():
+    from app.models.sim import Sim
+    return Sim.query.count()
+
+# Total de mediciones de señal registradas (GSM)
+def totalGSMEvents():
+    from app.models.gsm_event import GsmEvent
+    return GsmEvent.query.count()
+
+# Equipos por compania
+def totalDeviceForCarrier():
+    from app.models.sim import Sim
+    from app.models.device import Device
+    from sqlalchemy import func
+    from app import db
+    from app.models.device_sim import devices_sims
+    return Device.query.join(devices_sims).join(Sim).group_by(Sim.carrier_id).all()
---
+++ tests/report/total_gsm_event_registered.py
@@ -0,0 +1,39 @@
+from app import app, db
+from app.models.device import Device
+from app.models.gsm_event import GsmEvent
+from app.report.report import totalGSMEvents
+from tests import base_test_case
+
+
+class TotalSimsRegisteredTestCase(base_test_case.BaseTestCase):
+    '''
+    Unit tests for the API
+    '''
+
+    def populate(self):
+        # devices
+        device1 = Device(device_id="1")
+        device2 = Device(device_id="2")
+        device3 = Device(device_id="3")
+        device4 = Device(device_id="4")
+
+        # GSM events
+        event1 = GsmEvent()
+        event2 = GsmEvent()
+        event3 = GsmEvent()
+
+        device1.events = [event1, event2]
+        device2.events = [event3]
+        device3.events = []
+
+        db.session.add(device1)
+        db.session.add(device2)
+        db.session.add(device3)
+        db.session.add(device4)
+        db.session.commit()
+
+    # test de guardado de eventos: 1 wifi traffic event y 2 state change event
+    def test_two_devices(self):
+        with app.app_context():
+            total_gsm_events = totalGSMEvents()
+            assert total_gsm_events == 3
