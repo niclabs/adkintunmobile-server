5f85e24bbe534963b9afa39b737564c2e293a66c
zeeerooo <boris.rq@gmail.com>
2016-04-27 15:46:42 -0300

    Refactor de populate de user y carriers. Además, agregado populate de antennas, con respectivo archivo

---
+++ static/data/initial_data_antennas.py
@@ -0,0 +1,9 @@
+initial_data_antennas = '''
+{
+    "antennas":[
+     {"mcc": 730, "mnc": 3, "lac": 8772, "cid": 441589, "lat": -39.810023,"lon": -73.24377}
+     ]
+}
+'''
+
+#Agregar aquí las distintas antenas en el formato especificado para utilizarlas en la aplicación
