7b8239821b55c69cd87601dcc082ff31f9011865
zeeerooo <boris.rq@gmail.com>
2016-05-03 15:56:04 -0300

    Refactor templates, imports usando renders

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -6,7 +6,6 @@ from flask_admin import Admin
 init_login()
 
 admin = Admin(app, name='Adkintun', index_view=MyAdminIndexView(), base_template='admin/my_master.html', template_mode='bootstrap3')
-# admin = Admin(app, name='Adkintun', index_view=MyAdminIndexView(), base_template='my_master.html', template_mode='bootstrap3')
 
 from . import views
 
