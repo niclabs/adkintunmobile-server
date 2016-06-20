171761d9fe20e8f99701abfab1d7090ae79dfc9d
zeeerooo <boris.rq@gmail.com>
2016-06-20 16:32:06 -0400

    Eliminación de tabals de reportes, sus test y sus views

--- app/report/views.py
+++ app/report/views.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 
-from app import app, db
-from app.models.carrier import Carrier
+from app import app
 from app.report.report_generation import total_devices_reported, total_sims_registered, total_gsm_events, \
     total_device_for_carrier, total_sims_for_carrier, total_gsm_events_for_carrier
-from app.report.retrieve_processed_data import get_stored_data
 from config import Visualization
 from flask import render_template, request
 
@@ -27,8 +25,6 @@ def get_max_date(dateStr, format):
 
 @app.route('/report/')
 def report_index():
-    from app.report.report_generation import generate_report
-    generate_report()
     return render_template('report/index.html')
 
 
@@ -41,18 +37,9 @@ def total_devices_reported():
         min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
         max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
 
-    if real_time_visualization:
-
-        total_devices = total_devices_reported(min_date, max_date)
-        total_sims = total_sims_registered(min_date, max_date)
-        total_events = total_gsm_events(min_date, max_date)
-
-    else:
-        stored_data = get_stored_data(min_date, max_date)
-
-        total_devices = stored_data.total_devices
-        total_sims = stored_data.total_sims
-        total_events = stored_data.total_events
+    total_devices = total_devices_reported(min_date, max_date)
+    total_sims = total_sims_registered(min_date, max_date)
+    total_events = total_gsm_events(min_date, max_date)
 
     return render_template('report/totales.html', total_devices=total_devices, total_sims=total_sims,
                            total_events=total_events, min_date=min_date, max_date=max_date)
@@ -67,15 +54,7 @@ def device_for_carrier():
         min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
         max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
 
-    if real_time_visualization:
-
-        carriers = total_device_for_carrier(min_date, max_date)
-
-    else:
-        stored_data = get_stored_data(min_date, max_date)
-        carriers = db.session.query(Carrier).all()
-        for carrier in carriers:
-            carrier.devices_count = stored_data['devices_' + Carrier.carriers[carrier.name]]
+    carriers = total_device_for_carrier(min_date, max_date)
 
     return render_template('report/device_for_carrier.html', carriers=carriers,
                            min_date=min_date, max_date=max_date)
@@ -90,17 +69,10 @@ def sims_for_carrier():
         min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
         max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
 
-    if real_time_visualization:
-        carriers = total_sims_for_carrier(min_date, max_date)
-
-    else:
-        stored_data = get_stored_data(min_date, max_date)
-        carriers = db.session.query(Carrier).all()
-        for carrier in carriers:
-            carrier.sims_count = stored_data['sims_' + Carrier.carriers[carrier.name]]
+    carriers = total_sims_for_carrier(min_date, max_date)
 
     return render_template('report/sims_for_carrier.html', carriers=carriers,
-                               min_date=min_date, max_date=max_date)
+                           min_date=min_date, max_date=max_date)
 
 
 @app.route('/report/total_events_for_carrier')
@@ -112,14 +84,7 @@ def events_for_carrier():
         min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
         max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
 
-    if real_time_visualization:
-        carriers = total_gsm_events_for_carrier(min_date, max_date)
-
-    else:
-        stored_data = get_stored_data(min_date, max_date)
-        carriers = db.session.query(Carrier).all()
-        for carrier in carriers:
-            carrier.events_count = stored_data['events_' + Carrier.carriers[carrier.name]]
+    carriers = total_gsm_events_for_carrier(min_date, max_date)
 
     return render_template('report/events_for_carrier.html', carriers=carriers,
                            min_date=min_date, max_date=max_date)
