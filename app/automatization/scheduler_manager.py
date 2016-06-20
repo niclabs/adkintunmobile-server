3297d20b8a10952395d77ea5b4d06e1df832f474
zeeerooo <boris.rq@gmail.com>
2016-06-20 17:19:52 -0400

    Modificado scheduler para creación de registros una vez al mes

--- app/automatization/scheduler_manager.py
+++ app/automatization/scheduler_manager.py
@@ -1,17 +1,18 @@
 from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
 
-from app.report.report_generation import generate_report
+from app.report.report_generation import generate_json_general_reports
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def start_scheduler():
+    pass
     scheduler = BackgroundScheduler()
-    start_date=today_init_time()
-    scheduler.add_job(generate_report, 'interval', id="procesamiento diario", replace_existing=True, days=1,
-                      start_date=today_init_time())
-    print("Reportes seteados a las "+str(start_date))
+    start_date = get_first_day()
+    scheduler.add_job(generate_json_general_reports(), 'interval', id="procesamiento mensual", replace_existing=True,
+                      months=1,
+                      start_date=get_first_day())
     scheduler.start()
 
-def today_init_time():
-    return datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 
+def get_first_day():
+    return datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
