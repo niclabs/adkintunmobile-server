d05d39b2ce5193dc203ed329211e75076515e5eb
zeeerooo <boris.rq@gmail.com>
2016-06-21 16:01:48 -0400

    Creadas views para archivos json generados para general_reports, además de un fix en el método generador.

--- app/public/views.py
+++ app/public/views.py
@@ -1,11 +1,21 @@
-from flask import render_template
-
 from app import app
+from flask import render_template, json, jsonify
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/reports/general_reports/<year>/<month>')
+def general_reports(year, month):
+    try:
+        data = json.load(
+            open('reports/general_reports/' + year + '/general_report_' + month + '_' + year + '.json', 'r'))
+        return jsonify(data)
+    except Exception as e:
+        return page_not_found(e)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
