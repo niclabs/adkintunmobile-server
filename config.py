d93ae36a1c757b81316472a53e0d964bdd85c0ea
zeeerooo <boris.rq@gmail.com>
2016-06-30 15:30:15 -0400

    Fix: Delete Visualization global variable, and changed configuration management.

--- config.py
+++ config.py
@@ -1,38 +1,30 @@
-class Config(object):
+class DefaultConfig(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    USER = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://' + USER + ':' + SECRET_KEY + '@localhost/adkintun'
+    SECRET_KEY = "this-really-needs-to-be-changed"
+    USER = "this-really-needs-to-be-changed"
+    SQLALCHEMY_DATABASE_URI = "postgresql://" + USER + ":" + SECRET_KEY + "@localhost/this-really-needs-to-be-changed"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-class ProductionConfig(Config):
-    pass
 
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user_test:password_test@localhost/adkintun_test'
+class TestingConfig(DefaultConfig):
+    SQLALCHEMY_DATABASE_URI = "postgresql://user_test:password_test@localhost/adkintun_test"
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     TESTING = True
     DEBUG = True
 
-class AppTokens():
+
+class AppTokens:
     tokens = {
-        # tokens generados aleatoriamente, de largo 50
+        # random string, size 50 : name of the token
         "token-for-app-mobile": "app_mobile",
     }
 
 
-class AdminUser():
-    first_name = "Cambiar Usuario"
-    last_name = "Cambiar Usuario"
-    login = "Cambiar login"
-    email = "Crear correo"
-    password = "Cambiar pass"
-
-
-class Visualization():
-    real_time_info= False
+class AdminUser:
+    first_name = "this-really-needs-to-be-changed"
+    last_name = "this-really-needs-to-be-changed"
+    login = "this-really-needs-to-be-changed"
+    email = "this-really-needs-to-be-changed"
+    password = "this-really-needs-to-be-changed"
