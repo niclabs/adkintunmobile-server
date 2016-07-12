81b6a5b428a45707cb1446403d24fb670fe779a3
zeeerooo <boris.rq@gmail.com>
2016-07-12 16:15:32 -0400

    Fix: Change spanish to english in some test files.

--- tests/events/save_application_event_test.py
+++ tests/events/save_application_event_test.py
@@ -19,7 +19,7 @@ class SaveApplicationEventTestCase(base_test_case.BaseTestCase):
         '''
         populate()
 
-    # test de guardado de eventos: 1 wifi traffic event y 2 state change event
+    # Saving events test: 1 wifi traffic event y 2 state change event
     def test_save_normal_events(self):
         with app.app_context():
             token = list(AppTokens.tokens.keys())[0]
