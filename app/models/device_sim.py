9f9d8551302f14345d7c5e54acfe43d2420ead0d
zeeerooo <boris.rq@gmail.com>
2016-07-20 18:15:59 -0400

    Fix: Refactor models

--- app/models/device_sim.py
+++ app/models/device_sim.py
@@ -1,7 +1,7 @@
 from app import db
 from sqlalchemy import PrimaryKeyConstraint
 
-# Tabla para manejar la relacion many_to_many entre antenas y carriers entre devices y sims
+# Table to manage many to many relationship between device and sims
 devices_sims = db.Table('devices_sims',
                         db.Column('device_id', db.String(50), db.ForeignKey('devices.device_id')),
                         db.Column('sim_id', db.String(50), db.ForeignKey('sims.serial_number')),
