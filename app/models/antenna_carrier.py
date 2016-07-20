0bf192ab21446adeac407290716a3b16bc289fa4
zeeerooo <boris.rq@gmail.com>
2016-07-20 17:34:10 -0400

    Fix: Repaired link between gsm observation and antenna, carrier and telephony event, and carrier and antenna

--- app/models/antenna_carrier.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from sqlalchemy import PrimaryKeyConstraint
-
-# Tabla para manejar la relacion many_to_many entre antenas y carriers
-antennas_carriers = db.Table('antennas_carriers',
-                             db.Column('antenna_id', db.Integer, db.ForeignKey('antennas.id')),
-                             db.Column('carrier_id', db.Integer, db.ForeignKey('carriers.id')),
-                             PrimaryKeyConstraint('antenna_id', 'carrier_id', name='antenna_carrier_id'),
-                             )
--- app/models/gsm_event.py
+++ app/models/gsm_event.py
@@ -1,29 +1,27 @@
 from app import db
-from app.models.antenna_gsm_event import antennas_gsm_events
 from app.models.telephony_observation_event import TelephonyObservationEvent
 
 
 class GsmEvent(TelephonyObservationEvent):
     '''
-    Clase para los eventos de observacion de telefonía tipo Gsm
+    Gsm observation events model class
     '''
     __tablename__ = 'gsm_events'
     __mapper_args__ = {'polymorphic_identity': 'gsm_event'}
 
     id = db.Column(db.Integer, db.ForeignKey('telephony_observation_events.id'), primary_key=True)
-    gsm_cid = db.Column(db.Integer)
-    gsm_lac = db.Column(db.Integer)
     gsm_psc = db.Column(db.Integer)
     signal_ber_size = db.Column(db.Integer)
     signal_ber_mean = db.Column(db.Float)
     signal_ber_variance = db.Column(db.Float)
-    antennas = db.relationship('Antenna', secondary=antennas_gsm_events,
-                               backref=db.backref('gsm_events', lazy='dynamic'), lazy='dynamic')
+    gsm_lac = db.Column(db.Integer)
+    gsm_cid = db.Column(db.Integer)
+    antenna_id = db.Column(db.Integer, db.ForeignKey('antennas.id'))
 
     def __init__(self, date=None, app_version_code=None, sim_serial_number=None, device_id=None,
-                 telephony_standard=None, network_type=None, gsm_cid=None, gsm_lac=None, gsm_psc=None,
+                 telephony_standard=None, network_type=None, gsm_psc=None, gsm_cid=None, gsm_lac=None,
                  signal_strength_size=None, signal_strength_mean=None, signal_strength_variance=None,
-                 signal_ber_size=None, signal_ber_mean=None, signal_ber_variance=None):
+                 signal_ber_size=None, signal_ber_mean=None, signal_ber_variance=None, mnc=None, mcc=None):
         self.date = date
         self.app_version_code = app_version_code
         self.sim_serial_number = sim_serial_number
@@ -33,12 +31,14 @@ class GsmEvent(TelephonyObservationEvent):
         self.signal_strength_size = signal_strength_size
         self.signal_strength_mean = signal_strength_mean
         self.signal_strength_variance = signal_strength_variance
-        self.gsm_cid = gsm_cid
-        self.gsm_lac = gsm_lac
         self.gsm_psc = gsm_psc
         self.signal_ber_size = signal_ber_size
         self.signal_ber_mean = signal_ber_mean
         self.signal_ber_variance = signal_ber_variance
+        self.gsm_cid = gsm_cid
+        self.gsm_lac = gsm_lac
+        self.mnc = mnc
+        self.mcc = mcc
 
     def __repr__(self):
         return '<GsmEvent, id: %r, date: %r>' % (self.id, self.date)
