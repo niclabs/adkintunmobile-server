e6f891793d95f934d88e522ff2e24b37946a390c
zeeerooo <boris.rq@gmail.com>
2016-07-21 16:33:28 -0400

    Fix: Refactor to pep8 style

--- app/report/__init__.py
+++ app/report/__init__.py
@@ -1,4 +1,4 @@
-from app.report import views
 from app.report import antenna_signal_report_generation
 from app.report import general_report_generation
 from app.report import reports_generation
+from app.report import views
