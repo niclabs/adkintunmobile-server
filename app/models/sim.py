d4d4cb2fdf04e42ee31449c27c4fe2e726f1849c
zeeerooo <boris.rq@gmail.com>
2016-07-25 16:44:00 -0400

    Fix: Make a database commit after add all the events. Refactor function names also, to keep logic.

--- app/models/sim.py
+++ app/models/sim.py
@@ -4,17 +4,17 @@ from app.models.device_sim import devices_sims
 
 
 class Sim(base_model.BaseModel):
-    '''
+    """
     Clase tarjeta sim.
-    '''
-    __tablename__ = 'sims'
+    """
+    __tablename__ = "sims"
 
     serial_number = db.Column(db.String(50), primary_key=True)
     creation_date = db.Column(db.Date())
     carrier_id = db.Column(db.Integer, db.ForeignKey("carriers.id"))
-    devices = db.relationship('Device', secondary=devices_sims, backref=db.backref('sims', lazy='dynamic'),
-                              lazy='dynamic')
-    events = db.relationship('Event', backref='sim', lazy='dynamic')
+    devices = db.relationship("Device", secondary=devices_sims, backref=db.backref("sims", lazy="dynamic"),
+                              lazy="dynamic")
+    events = db.relationship("Event", backref="sim", lazy="dynamic")
 
     def __init__(self, serial_number=None, creation_date=None, carrier_id=None):
         self.serial_number = serial_number
@@ -22,21 +22,25 @@ class Sim(base_model.BaseModel):
         self.carrier_id = carrier_id
 
     def __repr__(self):
-        return '<Sim, serial_number: %r, creation_date: %r, carrier: %r, carrier_id: %r>' % \
+        return "<Sim, serial_number: %r, creation_date: %r, carrier: %r, carrier_id: %r>" % \
                (self.serial_number, self.creation_date, self.carrier, self.carrier_id)
 
     @staticmethod
-    def store_if_not_exist(args):
+    def get_sim_or_add_if_not_exist(args):
+        """
+        Search a sim and retrieve it if exist, else create a new one and retrieve it.
+        If there is not serial_number argument, return None
+        """
         from datetime import datetime
-        if 'serial_number' in args:
-            sim = Sim.query.filter(Sim.serial_number == args['serial_number']).first()
+        if "serial_number" in args:
+            sim = Sim.query.filter(Sim.serial_number == args["serial_number"]).first()
 
             if not sim:
-                sim = Sim(serial_number=args['serial_number'], creation_date=datetime.now())
+                sim = Sim(serial_number=args["serial_number"], creation_date=datetime.now())
                 db.session.add(sim)
             return sim
         else:
-            return None #NOOO, buscar y usar la sim vacia?
+            return None
 
     def add_device(self, device):
         from app.models.device import Device
