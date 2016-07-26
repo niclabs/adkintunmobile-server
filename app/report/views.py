e14e0b628399bb3c36ec39cf020097cbee8e5c1b
zeeerooo <boris.rq@gmail.com>
2016-07-26 17:49:53 -0400

    Fix: Add terms and conditions and refactor views in public and reports.

--- app/report/views.py
+++ app/report/views.py
@@ -1,87 +1,62 @@
-from datetime import datetime
+from datetime import timedelta
 
 from app import app
-from app.report.general_report_generation import total_devices_registred, total_sims_registered, total_gsm_events, \
-    total_device_for_carrier, total_sims_for_carrier, total_gsm_events_for_carrier
-from flask import render_template, request
+from flask import make_response, request, current_app
+from functools import update_wrapper
 
 
-def get_min_date(dateStr, format):
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):
+    if methods is not None:
+        methods = ", ".join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ", ".join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ", ".join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers["allow"]
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == "OPTIONS":
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != "OPTIONS":
+                return resp
+
+            h = resp.headers
+
+            h["Access-Control-Allow-Origin"] = origin
+            h["Access-Control-Allow-Methods"] = get_methods()
+            h["Access-Control-Max-Age"] = str(max_age)
+            if headers is not None:
+                h["Access-Control-Allow-Headers"] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+
+    return decorator
+
+
+from flask import json, jsonify
+from app.public.views import page_not_found
+
+
+@app.route("/reports/general_reports/<year>/<month>")
+@crossdomain(origin="*")
+def general_reports(year, month):
     try:
-        return datetime.strptime(dateStr, format).date()
-    except Exception:
-        pass
-
-
-def get_max_date(dateStr, format):
-    try:
-        return datetime.strptime(dateStr, format).date()
-    except Exception:
-        pass
-
-
-@app.route('/report/')
-def report_index():
-    return render_template('report/index.html')
-
-
-@app.route('/report/totales')
-def total_devices_reported_view():
-    min_date = None
-    max_date = None
-
-    if request.args:
-        min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
-        max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
-
-    total_devices = total_devices_registred(min_date, max_date)
-    total_sims = total_sims_registered(min_date, max_date)
-    total_events = total_gsm_events(min_date, max_date)
-
-    return render_template('report/totales.html', total_devices=total_devices, total_sims=total_sims,
-                           total_events=total_events, min_date=min_date, max_date=max_date)
-
-
-@app.route('/report/total_device_for_carrier')
-def device_for_carrier():
-    min_date = None
-    max_date = None
-
-    if request.args:
-        min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
-        max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
-
-    carriers = total_device_for_carrier(min_date, max_date)
-
-    return render_template('report/device_for_carrier.html', carriers=carriers,
-                           min_date=min_date, max_date=max_date)
-
-
-@app.route('/report/total_sims_for_carrier')
-def sims_for_carrier():
-    min_date = None
-    max_date = None
-
-    if request.args:
-        min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
-        max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
-
-    carriers = total_sims_for_carrier(min_date, max_date)
-
-    return render_template('report/sims_for_carrier.html', carriers=carriers,
-                           min_date=min_date, max_date=max_date)
-
-
-@app.route('/report/total_events_for_carrier')
-def events_for_carrier():
-    min_date = None
-    max_date = None
-
-    if request.args:
-        min_date = get_min_date(request.args["min_date"], "%Y-%m-%d")
-        max_date = get_max_date(request.args["max_date"], "%Y-%m-%d")
-
-    carriers = total_gsm_events_for_carrier(min_date, max_date)
-
-    return render_template('report/events_for_carrier.html', carriers=carriers,
-                           min_date=min_date, max_date=max_date)
+        data = json.load(
+            open("app/static/reports/general_reports/" + year + "/general_report_" + month + "_" + year + ".json",
+                 "r"))
+        return jsonify(data)
+    except Exception as e:
+        return page_not_found(e)
