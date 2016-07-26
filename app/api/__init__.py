027638137ee576801be9e59ee27be191e2ca5a03
zeeerooo <boris.rq@gmail.com>
2016-07-26 15:10:05 -0400

    Fix: Elimination of registration and registration test

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,8 @@
-from flask_restful import Api
-
 from app import app
+from flask_restful import Api
 
 # API stuff
 
 api = Api(app)
 app.config['BUNDLE_ERRORS'] = True
-from . import registration_view
 from . import events_view
