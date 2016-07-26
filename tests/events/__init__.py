4987443684c921f5110abb27660f1959beb5f000
zeeerooo <boris.rq@gmail.com>
2016-07-26 15:06:29 -0400

    Fix: Adding dynamically new telcos tests

--- tests/events/__init__.py
+++ tests/events/__init__.py
@@ -3,5 +3,5 @@ from tests.events.save_connectivity_event_test import SaveConnectivityEventTestC
 from tests.events.save_events_with_device_and_sim_test import SaveEventsWithDeviceAndSimTestCase
 from tests.events.save_gsm_observation_event_test import SaveGsmObservationEventTestCase
 from tests.events.save_mobile_event_test import SaveMobileEventTestCase
+from tests.events.save_new_telco_events import SaveNewTelcoEventsTestCase
 from tests.events.save_wifi_event_test import SaveWifiEventTestCase
-
---
+++ tests/events/save_new_telco_events.py
@@ -0,0 +1,52 @@
+from app import app
+from app.models.carrier import Carrier
+from app.models.gsm_event import GsmEvent
+from app.models.mobile_traffic_event import MobileTrafficEvent
+from app.models.sim import Sim
+from config import AppTokens
+from manage_commands import populate_test
+from tests import base_test_case
+from tests.events.new_telco_events import events_json
+
+
+class SaveNewTelcoEventsTestCase(base_test_case.BaseTestCase):
+    """
+    Unit tests for the API
+    """
+
+    def populate(self):
+        """
+        Populate the model with test data
+        """
+        # Create the default sim
+        populate_test()
+
+    # Saving event test: 1 gsm observation event and 1 state record, both whit a non-existent telco
+    def test_save_normal_events(self):
+        with app.app_context():
+            token = list(AppTokens.tokens.keys())[0]
+            request = self.app.post("/api/events", data=dict(
+                events=events_json
+            ), headers={"Authorization": "token " + token})
+
+            assert request.status_code == 201
+
+            gsm_events = GsmEvent.query.all()
+            assert len(gsm_events) == 1
+            gsm_event = gsm_events[0]
+
+            mobile_events = MobileTrafficEvent.query.all()
+            assert len(mobile_events) == 1
+
+            sim = Sim.query.filter(Sim.serial_number == "800000000000000000000").first()
+            assert sim
+
+            carrier = Carrier.query.filter(Carrier.mnc == 9, Carrier.mcc == 120).first()
+            assert carrier
+
+            assert sim.carrier.mnc == 9
+            assert sim.carrier.mcc == 120
+
+            assert len(carrier.telephony_observation_events.all()) == 1
+
+            assert carrier.telephony_observation_events[0].id == gsm_event.id
