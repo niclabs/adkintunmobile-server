fe8c41e5e2ba0ea586be065d1eb3f358bf640102
zeeerooo <boris.rq@gmail.com>
2016-07-28 15:02:55 -0400

    New api method to retrieve via GET the information of an antenna by its id.

--- app/models/antenna.py
+++ app/models/antenna.py
@@ -4,9 +4,9 @@ from sqlalchemy import UniqueConstraint
 
 
 class Antenna(base_model.BaseModel):
-    '''
+    """
     Antenna Class.
-    '''
+    """
     __tablename__ = "antennas"
     __table_args__ = (UniqueConstraint("cid", "lac", "carrier_id", name="antenna_pk"), {})
     id = db.Column(db.Integer, primary_key=True)
@@ -15,8 +15,8 @@ class Antenna(base_model.BaseModel):
     lat = db.Column(db.Float)
     lon = db.Column(db.Float)
     carrier_id = db.Column(db.Integer, db.ForeignKey("carriers.id"))
-    gsm_events = db.relationship('GsmEvent', backref='antenna',
-                                 lazy='dynamic')
+    gsm_events = db.relationship("GsmEvent", backref="antenna",
+                                 lazy="dynamic")
 
     def __init__(self, cid=None, lac=None, lat=None, lon=None):
         self.cid = cid
@@ -26,3 +26,15 @@ class Antenna(base_model.BaseModel):
 
     def __repr__(self):
         return "<Antenna, id: %r,  cid: %r, lac: %r, carrier: %r,>" % (self.id, self.cid, self.lac, self.carrier_id)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            "id": self.id,
+            "cid": self.cid,
+            "lac": self.lac,
+            "lat": self.lat,
+            "lon": self.lon,
+            "carrier_id": self.carrier_id
+        }
