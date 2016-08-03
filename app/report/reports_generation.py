229ae8a28818a6290da33cd86df55d015fbbcf3f
zeeerooo <boris.rq@gmail.com>
2016-08-03 13:09:35 -0400

    Add signal report generation  testing

--- app/report/reports_generation.py
+++ app/report/reports_generation.py
@@ -8,6 +8,7 @@ from flask import json
 def monthly_reports_generation():
     from app.report.general_report_generation import generate_json_general_reports
     from app.report.antenna_network_report_generation import generate_json_network_reports
+    from app.report.antenna_signal_report_generation import generate_json_signal_reports
     # get month for the report
     actual_month = datetime.now().month
     actual_year = datetime.now().year
@@ -21,9 +22,9 @@ def monthly_reports_generation():
     init_date = datetime(year=year_new_report, month=month_new_report, day=1)
     last_date = datetime(year=actual_year, month=actual_month, day=1, hour=23, minute=59, second=59) - timedelta(days=1)
 
-    generate_json_general_reports(init_date, last_date)
-    generate_json_network_reports(init_date, last_date)
-
+    # generate_json_general_reports(init_date, last_date)
+    # generate_json_network_reports(init_date, last_date)
+    generate_json_signal_reports(init_date, last_date)
 
 def save_json_report_to_file(json_data: dict, year: int, month: int, folder: str, name: str):
     """
