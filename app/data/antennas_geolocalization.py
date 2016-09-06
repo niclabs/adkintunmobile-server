4c7265a6fa10b3a597f7959a1925984999ed114a
zeeerooo <boris.rq@gmail.com>
2016-09-06 16:42:10 -0300

    Created method to add geolocalizated antennas to database

--- app/data/antennas_geolocalization.py
+++ app/data/antennas_geolocalization.py
@@ -1,5 +1,7 @@
 import requests
 
+from app import db
+
 BASE_URL = "http://opencellid.org/cell/get"
 
 
@@ -14,11 +16,12 @@ def get_antenna_geolocalization(mcc: int, mnc: int, lac: int, cid: int, key_id: 
     :param base_url: OpenCellId url
     :return: Tuple Lat, Long returned from OpenCellId
     """
-    url = BASE_URL + "?key=" + key_id + "&mnc=" + str(mnc) + "&mcc="+ str(mcc) + "&cellid=" + str(cid) + "&lac=" + str(lac)+"&format=json"
+    url = BASE_URL + "?key=" + key_id + "&mnc=" + str(mnc) + "&mcc=" + str(mcc) + "&cellid=" + str(cid) + "&lac=" + str(
+        lac) + "&format=json"
     r = requests.get(url)
     if r.status_code == 200:
-         json = r.json()
-         return json["lat"], json["lon"]
+        json = r.json()
+        return json["lat"], json["lon"]
     else:
         return None, None
 
@@ -31,9 +34,22 @@ def update_antennas_Localization(max_number_of_queries: int) -> int:
     :param max_number_of_queries: Max number of queries to make for each call
     :return: Number of antennas updated
     """
-    return 0
+    from app.models.antenna import Antenna
+    from app.models.carrier import Carrier
+    from config import OpenCellIdToken
 
+    antennas = Antenna.query.filter(Antenna.lat == None, Antenna.lon == None).limit(max_number_of_queries)
 
-"""
-http://opencellid.org/cell/get?key=579b4655-a318-433c-ad94-b71261313722&mcc=730&mnc=2&lac=55700&cellid=1259355
-"""
+    upload_antennas = 0
+    for antenna in antennas:
+        carrier = Carrier.query.filter(Carrier.id == antenna.carrier_id).first()
+        lat, lon = get_antenna_geolocalization(mcc=carrier.mcc, mnc=carrier.mnc, lac=antenna.lac, cid=antenna.cid,
+                                               key_id=OpenCellIdToken.token, base_url=BASE_URL)
+        if lat and lon:
+            antenna.lat = lat
+            antenna.lon = lon
+            db.session.add(antenna)
+            db.session.commit()
+            upload_antennas += 1
+
+    return upload_antennas
