ba19c2d7c977cead06bdb8ed39e7446ae8634733
zeeerooo <boris.rq@gmail.com>
2016-09-06 18:14:00 -0300

    setted new job for antennas geolocalization

--- app/automatization/scheduler_manager.py
+++ app/automatization/scheduler_manager.py
@@ -1,14 +1,19 @@
 from datetime import datetime
 
-from app.report.reports_generation import monthly_reports_generation
 from apscheduler.schedulers.background import BackgroundScheduler
+
+from app.data.antennas_geolocalization import update_antennas_localization
+from app.report.reports_generation import monthly_reports_generation
 
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
     start_date = get_first_day()
-    scheduler.add_job(monthly_reports_generation, "cron", id="procesamiento mensual", replace_existing=True,
+    scheduler.add_job(monthly_reports_generation, trigger="cron", id="Monthly reports generation",
+                      replace_existing=True,
                       day="1", hour="0", minute="0", start_date=start_date)
+    scheduler.add_job(update_antennas_localization, kwargs={"max_number_of_queries": 1000}, trigger="cron",
+                      id="Antennas geolocalization", replace_existing=True, hour=3, minute=0)
     scheduler.start()
 
 
