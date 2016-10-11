106a4c037ab26c844d9243f56187335cda487a69
zeeerooo <boris.rq@gmail.com>
2016-10-11 15:37:24 -0300

    Error and created messages added for mules process generation

--- app/__init__.py
+++ app/__init__.py
@@ -8,19 +8,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import AppTokens
 
-
 # Create flask app
 app = Flask(__name__)
 
 # Load the default configuration
-app.config.from_object('config.DefaultConfig')
+app.config.from_object("config.DefaultConfig")
 
 # Define the database object which is imported
 # by modules and controllers
 db = SQLAlchemy(app)
 
 # Authetication scheme
-auth = HTTPTokenAuth(scheme='Token')
+auth = HTTPTokenAuth(scheme="Token")
 
 
 @auth.verify_token
@@ -42,24 +41,24 @@ from app import public
 from app import admin
 from app import report
 
-
 # Create log files
 if not app.debug:
-    log_folder = 'tmp/'
-    log_filename = 'adkintun-error.log'
+    log_folder = "tmp/"
+    log_filename = "adkintun-debug.log"
     if not os.path.exists(log_folder):
         os.makedirs(log_folder)
 
-    file_handler = RotatingFileHandler(log_folder + log_filename, 'a', maxBytes=50 * 1024 * 1024)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler = RotatingFileHandler(log_folder + log_filename, maxBytes=50 * 1024 * 1024)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
-    app.logger.info('Adkintun log start')
+    app.logger.info("Adkintun log start")
 
 # start uwsgi cron jobs for antennas geolocalization and reports generation
 # Just run in a uwsgi instance!
 try:
+    app.logger.info("Uwsgi mules created for antennas geolocalization and reports generation")
     import app.automatization.scheduler_manager
 except:
-    pass
+    app.logger.error("Problem with the mules")
