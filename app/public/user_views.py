cd7dac8141b269e9f08ed90933cb537370d0ca80
Diego Madariaga <diego@niclabs.cl>
2017-03-16 11:21:38 -0300

    Add methods to handle websockets communication

--- app/public/user_views.py
+++ app/public/user_views.py
@@ -1,5 +1,6 @@
-from app import application
 from flask_cors import cross_origin
+
+from app import application
 
 seconds_in_a_day = 60 * 60 * 24
 
@@ -53,9 +54,9 @@ def get_gsm_events(device_id, selected_day):
 
     return jsonify(ret), 200
 
-@application.route("/app_traffic/<string:device_id>/<int:from_day>/<int:to_day>", methods=["GET"])
+@application.route("/app_traffic/<string:device_id>/<int:actual_day>", methods=["GET"])
 @cross_origin()
-def get_app_traffic(device_id, from_day, to_day):
+def get_app_traffic(device_id, actual_day):
     from flask import jsonify
     from datetime import datetime
     from app import db
@@ -68,13 +69,12 @@ def get_app_traffic(device_id, from_day, to_day):
                                    db.func.sum(ApplicationTrafficEvent.rx_bytes).label('rx_bytes'),
                                    db.func.sum(ApplicationTrafficEvent.tx_bytes).label('tx_bytes'),
                                    ApplicationTrafficEvent.network_type) \
-        .filter(ApplicationTrafficEvent.device_id == device_id, ApplicationTrafficEvent.application_id == Application.id) \
+        .filter(ApplicationTrafficEvent.device_id == device_id, ApplicationTrafficEvent.network_type == 1, ApplicationTrafficEvent.application_id == Application.id, ApplicationTrafficEvent.date >= datetime.fromtimestamp(actual_day - seconds_in_a_day*30)) \
         .group_by(Application.id, ApplicationTrafficEvent.network_type).all()
     ret = [{'app_id' : i.id,
             'app_name': i.name,
             'rx_bytes' : float(i.rx_bytes),
-            'tx_bytes' : float(i.tx_bytes),
-            'network_type': i.network_type} for i in app_traffic]
+            'tx_bytes' : float(i.tx_bytes)} for i in app_traffic]
 
     return jsonify(ret), 200
 
@@ -97,6 +97,26 @@ def get_monthly_app_traffic(device_id, actual_day, app_id):
 
     return jsonify(ret), 200
 
+def getNetworkType(id):
+    from app.models.speedtests.network_interface import NetworkInterface
+
+    try:
+        return NetworkInterface.query.get(id).active_interface
+    except (TypeError, AttributeError):
+        return -1
+
+def getNetworkTypeDetails(id):
+    from app.models.speedtests.network_interface import NetworkInterface
+
+    try:
+        ni = NetworkInterface.query.get(id)
+        if ni.active_interface == 1:
+            return ni.network_type
+        else:
+            return ni.ssid
+    except (TypeError, AttributeError):
+        return -1
+
 @application.route("/speed_test/<string:device_id>", methods=["GET"])
 @cross_origin()
 def get_speed_test_reports(device_id):
@@ -104,12 +124,48 @@ def get_speed_test_reports(device_id):
     from app.models.speedtests.speed_test_report import SpeedTestReport
 
     reports = SpeedTestReport.query.filter(SpeedTestReport.device_id == device_id).all()
-    ret = [{'network_interface_id' : i.network_interface_id,
+    ret = {'data': [{
+            'network_type' : getNetworkType(i.network_interface_id),
+            'details' : getNetworkTypeDetails(i.network_interface_id),
             'host' : i.host,
             'download_speed': i.download_speed,
             'download_size': i.download_size,
             'upload_speed': i.upload_speed,
             'upload_size': i.upload_size,
-            'date': i.date.timestamp()} for i in reports]
+            'date': i.date.timestamp()} for i in reports]}
 
+    return jsonify(ret), 200
+
+@application.route("/media_test/<string:device_id>", methods=["GET"])
+@cross_origin()
+def get_media_test_reports(device_id):
+    from flask import jsonify
+    from app.models.speedtests.media_test_report import MediaTestReport
+
+    reports = MediaTestReport.query.filter(MediaTestReport.device_id == device_id).all()
+    ret = {'data': [{
+            'network_type' : getNetworkType(i.network_interface_id),
+            'details' : getNetworkTypeDetails(i.network_interface_id),
+            'results' : [{'quality' : j.quality,
+                          'buffering_time' : j.buffering_time,
+                          'loaded_fraction' : j.loaded_fraction,
+                          'downloaded_bytes' : j.downloaded_bytes} for j in i.video_results.all()],
+            'date': i.date.timestamp()} for i in reports]}
+    return jsonify(ret), 200
+
+@application.route("/connectivity_test/<string:device_id>", methods=["GET"])
+@cross_origin()
+def get_connectivity_test_reports(device_id):
+    from flask import jsonify
+    from app.models.speedtests.connectivity_test_report import ConnectivityTestReport
+
+    reports = ConnectivityTestReport.query.filter(ConnectivityTestReport.device_id == device_id).all()
+    ret = {'data': [{
+            'network_type' : getNetworkType(i.network_interface_id),
+            'details' : getNetworkTypeDetails(i.network_interface_id),
+            'results' : [{'url' : j.url,
+                          'loaded' : j.loaded,
+                          'loading_time' : j.loading_time,
+                          'downloaded_bytes' : j.downloaded_bytes} for j in i.sites_results.all()],
+            'date': i.date.timestamp()} for i in reports]}
     return jsonify(ret), 200
